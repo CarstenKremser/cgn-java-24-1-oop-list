1b2cb20d7e8dfd732db2dad0f8b78440e8113ca9
Carsten Kremser <github@carstenkremser.de>
2024-03-18 15:02:58 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Student> students = new ArrayList<Student>();
+
+        students.add(new Student("a001","Ansgar", "Apfel"));
+        students.add(new Student("b001","Berta","Brandt"));
+        students.add(new Student("c001","Claudia","Cactus"));
+        students.add(new Student("d001","Dora","Dingsda"));
+        students.add(new Student("e001","Ernst","Einhorn"));
+
+        for(Student student:students) {
+            System.out.println(student);
+        }
+
+
+    }
+}
